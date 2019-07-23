8c026d10b8764e30f24904fa16650f0b58465812
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-07-23 20:54:33 +0530

    Updates

---
+++ precache-manifest.c233869e7a808d468de63735b2712ff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66af37f5bca5484ec13de1ee1663e707",
+    "url": "/biz-review/index.html"
+  },
+  {
+    "revision": "ae1463fd12da86f87756",
+    "url": "/biz-review/static/css/main.7243efb3.chunk.css"
+  },
+  {
+    "revision": "b8e185938b0483f28b8f",
+    "url": "/biz-review/static/js/2.34fa636c.chunk.js"
+  },
+  {
+    "revision": "ae1463fd12da86f87756",
+    "url": "/biz-review/static/js/main.8a197f23.chunk.js"
+  },
+  {
+    "revision": "9b22f16cfb9d6c69f60a",
+    "url": "/biz-review/static/js/runtime~main.63bee1cf.js"
+  }
+]);
